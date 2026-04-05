02f1644028631162c19a475573a349ff92d154d8
Valtteri858 <116634399+Valtteri858@users.noreply.github.com>
2026-04-05 15:36:11 +0300

    Delete src directory

--- src/components/Banner.jsx
+++
@@ -1,8 +0,0 @@
-export default function Banner() {
-  return (
-    <figure>
-      <img src="/kuvat/Tarjous.jpg" alt="Viikon tarjousbanneri" width="1200" height="400" />
-      <figcaption>Viikon parhaat tarjoukset</figcaption>
-    </figure>
-  );
-}
